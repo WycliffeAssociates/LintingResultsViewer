27458324e3ba1aaf9ca245b62de01532b01ae267
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2024-11-22 16:56:44 -0500

    Fix the db in container fiasco

--- LintingResults/Program.cs
+++ LintingResults/Program.cs
@@ -75,6 +75,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDbContext>();
     context.Database.Migrate();
+    var lintingContext = services.GetRequiredService<LintingDbContext>();
+    lintingContext.Database.Migrate();
 }
 
 app.Run();
