5d04f6cff87c131865d0b9f0286dc35d82a73141
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2026-05-09 09:48:08 -0400

    Use a commit id to deduplicate and merge incoming result

--- LintingResults/Services/LintingResultListener.cs
+++ LintingResults/Services/LintingResultListener.cs
@@ -41,56 +41,91 @@ public class LintingResultListener: IHostedService
         using var scope = _serviceScopeFactory.CreateScope();
         var lintingDbContext = scope.ServiceProvider.GetRequiredService<LintingDbContext>();
         var lintingResult = JsonSerializer.Deserialize(arg.Message.Body, JSONContext.Default.LintingResult);
-        var matchingRepo = lintingDbContext.Repos
-            .FirstOrDefault(i => i.RepoId == lintingResult.RepoId);
         _logger.LogDebug($"Received linting result for {lintingResult.User}/{lintingResult.Repo}");
-        var repoLintingItems = new Dictionary<string, Dictionary<string, List<LintingResultItem>>>();
-        using var result = await _httpClient.GetAsync(lintingResult.ResultsFileUrl);
-        if (!result.IsSuccessStatusCode)
+
+        using var response = await _httpClient.GetAsync(lintingResult.ResultsFileUrl);
+        if (!response.IsSuccessStatusCode)
         {
             _logger.LogError($"Failed to download results file from {lintingResult.ResultsFileUrl}");
-            repoLintingItems = null;
+            return;
         }
-        else
+        var repoLintingItems = await response.Content.ReadFromJsonAsync(JSONContext.Default.DictionaryStringDictionaryStringListLintingResultItem);
+
+        if (lintingResult.CommitId != null)
         {
-            repoLintingItems = await result.Content.ReadFromJsonAsync(JSONContext.Default.DictionaryStringDictionaryStringListLintingResultItem);
+            var existingForCommit = lintingDbContext.LintingResults
+                .FirstOrDefault(r => r.RepoId == lintingResult.RepoId && r.CommitId == lintingResult.CommitId);
+            if (existingForCommit != null)
+            {
+                MergeInto(existingForCommit.LintingItems, repoLintingItems);
+                lintingDbContext.Entry(existingForCommit).Property(x => x.LintingItems).IsModified = true;
+                await lintingDbContext.SaveChangesAsync();
+                return;
+            }
         }
+
+        var matchingRepo = lintingDbContext.Repos.FirstOrDefault(i => i.RepoId == lintingResult.RepoId);
         if (matchingRepo == null)
         {
-            var newRepo = new Repo
+            lintingDbContext.Repos.Add(new Repo
             {
-                
                 RepoId = lintingResult.RepoId,
                 RepoName = lintingResult.Repo,
                 User = lintingResult.User,
-                LintingResults = new List<LintingResultDBModel>()
-            };
-            if (repoLintingItems != null)
-            {
-
-                newRepo.LintingResults.Add(
-                    new()
-                    {
-                        dateInserted = DateTime.Now,
-                        LintingItems = repoLintingItems
-                    });
-            }
-            lintingDbContext.Repos.Add(newRepo);
+                LintingResults = new List<LintingResultDBModel>
+                {
+                    new() { dateInserted = DateTime.Now, CommitId = lintingResult.CommitId, LintingItems = repoLintingItems }
+                }
+            });
         }
         else
         {
-            if (repoLintingItems != null)
+            matchingRepo.LintingResults ??= new List<LintingResultDBModel>();
+            matchingRepo.LintingResults.Add(new()
             {
-                matchingRepo.LintingResults ??= new List<LintingResultDBModel>();
-                matchingRepo.LintingResults.Add(new()
-                {
-                    dateInserted = DateTime.Now,
-                    LintingItems = repoLintingItems
-                });
-            }
+                dateInserted = DateTime.Now,
+                CommitId = lintingResult.CommitId,
+                LintingItems = repoLintingItems
+            });
         }
 
         await lintingDbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Merges linting results
+    /// </summary>
+    /// <param name="existing">The already existing result</param>
+    /// <param name="incoming">The new one we have coming in</param>
+    /// <remarks>This will merge things into a chapter -> verse hierarchy while not inserting duplicates</remarks>
+    private static void MergeInto(
+        Dictionary<string, Dictionary<string, List<LintingResultItem>>> existing,
+        Dictionary<string, Dictionary<string, List<LintingResultItem>>> incoming)
+    {
+        foreach (var (book, incomingChapters) in incoming)
+        {
+            if (!existing.TryGetValue(book, out var existingChapters))
+            {
+                existing[book] = incomingChapters;
+                continue;
+            }
+            foreach (var (chapter, incomingItems) in incomingChapters)
+            {
+                if (!existingChapters.TryGetValue(chapter, out var existingItems))
+                {
+                    existingChapters[chapter] = incomingItems;
+                    continue;
+                }
+                var existingKeys = existingItems.Select(i => (i.verse, i.errorId)).ToHashSet();
+                foreach (var item in incomingItems)
+                {
+                    if (existingKeys.Add((item.verse, item.errorId)))
+                    {
+                        existingItems.Add(item);
+                    }
+                }
+            }
+        }
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
@@ -106,4 +141,5 @@ public class LintingResult
     public string User { get; set; }
     public string Repo { get; set; }
     public string ResultsFileUrl { get; set; }
+    public string? CommitId { get; set; }
 }
